c6b335b2e7f2b5c50794959b525b3a7376669c0c
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-08 14:40:42 -0300

    refactor: change material from enum to model

--- src/main/java/br/com/mercadolivre/storage_implementation/model/Material.java
+++ src/main/java/br/com/mercadolivre/storage_implementation/model/Material.java
@@ -1,8 +1,24 @@
 package br.com.mercadolivre.storage_implementation.model;
 
-public enum Material {
-    GOLD,
-    SILVER,
-    BRONZE,
-    VITRAL
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+public class Material {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private BigDecimal value;
 }
